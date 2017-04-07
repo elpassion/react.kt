e06e542eb85da5c78f155cce5f23194c8ba2b64f
Paweł Gajda <pawegio@gmail.com>
2017-04-07 14:47:46 +0200

    Cleanup

--- frontend/src/org/jetbrains/firebase/database.kt
+++ frontend/src/org/jetbrains/firebase/database.kt
@@ -1,6 +1,6 @@
 package org.jetbrains.firebase
 
-private external val db : dynamic
+private external val db: dynamic
 
 fun writeTodoListState(id: String, state: Any?) {
     db.child("todo-list").child(id).set(state)
@@ -14,10 +14,10 @@ fun subscribeToTodoListState(id: String, callback: (FirebaseUpdate) -> Unit) {
     db.child("todo-list").child(id).on("value", callback)
 }
 
-external class FirebaseUpdate {
-    fun `val`(): Array<String>
-}
-
 fun subscribeToTicTacToeState(id: String, callback: (FirebaseUpdate) -> Unit) {
     db.child("tic-tac-toe").child(id).on("value", callback)
+}
+
+external class FirebaseUpdate {
+    fun `val`(): Array<String>
 }
