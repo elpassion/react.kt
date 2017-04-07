5ef296f713061d3fa38b02797e8d692381ddcb57
Paweł Gajda <pawegio@gmail.com>
2017-04-07 14:47:15 +0200

    Remove optional semicolons

--- frontend/src/org/jetbrains/firebase/database.kt
+++ frontend/src/org/jetbrains/firebase/database.kt
@@ -3,22 +3,21 @@ package org.jetbrains.firebase
 private external val db : dynamic
 
 fun writeTodoListState(id: String, state: Any?) {
-    db.child("todo-list").child(id).set(state);
+    db.child("todo-list").child(id).set(state)
 }
 
 fun writeTicTacToeState(id: String, state: Any?) {
-    db.child("tic-tac-toe").child(id).set(state);
+    db.child("tic-tac-toe").child(id).set(state)
 }
 
 fun subscribeToTodoListState(id: String, callback: (FirebaseUpdate) -> Unit) {
-    db.child("todo-list").child(id).on("value", callback);
+    db.child("todo-list").child(id).on("value", callback)
 }
 
 external class FirebaseUpdate {
-
     fun `val`(): Array<String>
 }
 
 fun subscribeToTicTacToeState(id: String, callback: (FirebaseUpdate) -> Unit) {
-    db.child("tic-tac-toe").child(id).on("value", callback);
+    db.child("tic-tac-toe").child(id).on("value", callback)
 }
