f8d1b8d583953424fc1649e1fc94585a160f96da
Paweł Gajda <pawegio@gmail.com>
2017-04-07 13:21:04 +0200

    Cleanup application

--- frontend/src/org/jetbrains/demo/thinkter/model/responses.kt
+++
@@ -1,6 +0,0 @@
-package org.jetbrains.demo.thinkter.model
-
-interface RpcData
-
-data class IndexResponse(val top: List<Thought>, val latest: List<Thought>) : RpcData
-data class PostThoughtToken(val user: String, val date: Long, val code: String) : RpcData
